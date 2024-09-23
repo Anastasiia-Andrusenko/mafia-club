8a7aa510e2dfa4c25376bc5a07c8a74b2cdb100d
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-09-23 18:59:02 +0300

    Updates

---
+++ _next/static/surcWXhaWXtzZ1amaNY2E/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/39583da2043c5039.css","static/chunks/pages/index-52f10110df9a51b8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
